715abb035d899aa1005a2d982a51cfae0bb0672d
Jorge Flores <jflores@elementum.com>
2020-02-03 14:56:28 -0300

    removing unused import

--- src/workast/model/User.js
+++ src/workast/model/User.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const validator = require('../validator/Validators');
-require('mongoose-type-url');
-
 
 const userSchema = new mongoose.Schema({
     name:   {type:String, required:true,validate: validator.stringValidator},
